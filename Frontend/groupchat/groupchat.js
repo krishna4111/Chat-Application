e61573c1412a463f5d8b43dbaf319e6df5a9b216
krishna4111 <krishnak4111@gmail.com>
2023-07-02 18:14:13 +0530

    admine

--- Frontend/groupchat/groupchat.js
+++ Frontend/groupchat/groupchat.js
@@ -1,8 +1,10 @@
-async function sendMessage(){
+async function sendMessage(e){
     try{
+      e.preventDefault();
         const token=localStorage.getItem('token');
         const msg=document.getElementById('msg-bar').value;
         const groupname=localStorage.getItem('groupname');
+        console.log('i am groupname',groupname)
         obj={
             msg,
             groupname
@@ -13,7 +15,7 @@ async function sendMessage(){
       response.data.chatDetails.username='you';
       showMessageOnScreen(response.data.chatDetails)
       document.getElementById('msg-bar').value = "";
-     // location.reload();
+     location.reload();
     }
     catch(err){
         console.log(err);
@@ -45,8 +47,16 @@ h4.innerHTML=`Group Name : ${groupname}`;
     
     
   const response =  await axios.get(`http://localhost:3000/groupchat/show-all/${groupname}`,{headers:{'Authorization':token}},groupname);
+console.log(response.data.showUsers);
+  if(response.data.usergroup[0].isAdmine === false){
+    document.getElementById("addmember").style.visibility="hidden";
+    document.getElementById("makeadmine").style.visibility="hidden";
+    document.getElementById("showuser").style.visibility="hidden";
 
-  
+  }
+  else{
+    showAllUsers();
+  }
   let array=new Array();
   if(response.data.chat.length > 10){
     let n=response.data.chat.length-1;
@@ -71,7 +81,90 @@ h4.innerHTML=`Group Name : ${groupname}`;
   }
   showMessageOnScreen(ele);
   })
+
+
+  
+ 
+
 })
+
+
+async function showAllUsers(){
+  try{
+    console.log('i am inside')
+    const token=localStorage.getItem('token');
+    const groupname=localStorage.getItem('groupname')
+  
+    const response=await axios.get(`http://localhost:3000/groupchat/show-all-users/${groupname}`,{headers:{'Authorization':token}});
+    console.log(response.data);
+    response.data.getusers.forEach(user=>{
+      showUsers(user.name,user.userId)
+    })
+    
+  }
+  catch(err){
+    console.log(err);
+  }
+
+}
+
+function showUsers(name,id){
+  const parentNode = document.getElementById("showing");
+  const createNewUser = `<li style="margin-left: 110px;" id=${id}> Name : ${name} 
+  <button style="padding:3px;margin:5px" onclick=removeUserFromGroup('${id}') class="btn btn-danger">Remove</button>
+  
+      </li>`;
+      parentNode.innerHTML += createNewUser;
+}
+
+async function removeUserFromGroup(userId) {
+  try{
+    const groupname=localStorage.getItem('groupname');
+    const token = localStorage.getItem("token");
+    await axios.delete(`http://localhost:3000/groupchat/remove-user/${userId}/${groupname}`, { headers: { 'Authorization': token } })
+      
+        removeItemFromScreen(userId);
+        location.reload();
+    
+  }
+  catch(err){
+    console.log(err);
+  }
+  
+}
+function removeItemFromScreen(UserId) {
+  try{
+    const parentNode = document.getElementById("showing");
+    const elem = document.getElementById(UserId);
+    parentNode.removeChild(elem);
+  }
+ 
+  catch(err){
+    console.log(err);
+  }
+}
+
+
+async function makeAdmine(e){
+  try{
+    const token=localStorage.getItem('token');
+    const groupname=localStorage.getItem('groupname');
+    const email=document.getElementById('admine').value;
+    obj={
+      email,
+      groupname
+    }
+  const admin=  await axios.post('http://localhost:3000/groupchat/made-admine',obj,{headers:{'Authorization':token}})
+  alert(admin.data.message);
+  location.reload();
+
+  }
+  catch(err){
+    console.log(err)
+  }
+}
+
+
 
 function redirectaddmembers(){
     window.location.href="../addmembers/addmember.html"
--- backend/controller/groupchat.js
+++ backend/controller/groupchat.js
@@ -9,7 +9,7 @@ const sendMessage=async (req,res)=>{
     const t = await sequelize.transaction();
     try{
        const{msg,groupname}=req.body;
-       const group=await Group.findOne({groupname});
+       const group=await Group.findOne({where:{groupname}});
        const chatDetails= await Chat.create({msg ,username:req.user.name, userId:req.user.id,groupId:group.id});
        res.status(200).json({success:true , chatDetails});
        await t.commit();
@@ -28,7 +28,8 @@ const showAllChat=async (req,res)=>{
         const groupname=req.params.groupname;
         const group= await Group.findOne({where:{groupname}});
         const chat=await Chat.findAll({where:{groupId:group.id}});
-        res.status(200).json({chat});
+        const usergroup=await UserGroup.findAll({where:{userId:req.user.id , groupId:group.id}});
+        res.status(200).json({chat , usergroup});
        
     }
     catch(err){
@@ -37,9 +38,63 @@ const showAllChat=async (req,res)=>{
     }
 }
 
+const makeAdmine=async (req,res)=>{
+    try{
+        const{email,groupname}=req.body;
+
+        const group=await Group.findOne({where:{groupname}});
+        const user=await User.findOne({where:{email}});
+        
+        const admine=await UserGroup.update({isAdmine:true},{where:{userId:user.id , groupId:group.id}});
+        res.status(201).json({success:true, message:`${user.name} is a admine now`})
+    }
+    catch(err){
+        console.log(err);
+        res.status(202).json({success:false,error:err});
+    }
+   
+}
+
+const ShowAllUsers=async (req,res)=>{
+    try{
+        const groupname=req.params.groupname;
+    
+        const group=await Group.findOne({where:{groupname}});
+        console.log
+        const getusers=await UserGroup.findAll({
+            attributes:['name','userId'],
+            where:{groupId:group.id}
+        })
+        res.status(200).json({getusers});
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({error:err});
+    }
+    
+
+
+}
+const removeUser=async (req,res)=>{
+    try{
+   const userId=req.params.userId;
+   const groupname=req.params.groupname;
+   const group=await Group.findOne({where:{groupname}});
+   const remove=await UserGroup.destroy({where:{groupId:group.id , userId:userId}});
+   res.status(201).json({message:'user removed successfuly'});
+    }
+    catch(err){
+     console.log(err);
+     res.status(500).json({message:'somethig went wrong', error:err});
+    }
+}
+
 
 
 module.exports={
     sendMessage,
-    showAllChat
+    showAllChat,
+    makeAdmine,
+    ShowAllUsers,
+    removeUser
 }
